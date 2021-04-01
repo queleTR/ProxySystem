d762d002c06c17c671e5b980b97662d88ba4e7ee
quele-official <queleyt2005@gmail.com>
2021-04-01 23:34:23 +0200

    ja

--- src/main/java/de/quele/proxysystem/commands/VerifyCommand.java
+++ src/main/java/de/quele/proxysystem/commands/VerifyCommand.java
@@ -6,12 +6,15 @@
 
 package de.quele.proxysystem.commands;
 
+import de.proxy.hypedcbot.discord.DiscordManager;
 import de.quele.proxysystem.ProxySystem;
 import de.quele.proxysystem.utils.RangSQL;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+
+import java.util.Objects;
 
 public class VerifyCommand extends Command {
 
@@ -26,7 +29,10 @@ public class VerifyCommand extends Command {
 
             if (strings.length == 1) {
                 RangSQL.setDiscordId(player.getUniqueId().toString(), strings[0]);
-                player.sendMessage(new TextComponent("Discordregister durchgeführt"));
+                Objects.requireNonNull(DiscordManager.jda.
+                        getUserById(de.proxy.hypedcbot.discord.mysql.RangSQL.getDiscordId(player.getUniqueId().toString()))).openPrivateChannel().queue(privateChannel -> {
+                    privateChannel.sendMessage("Please write #verify").queue();
+                });
             } else {
                 player.sendMessage(new TextComponent(ProxySystem.getProxySystem().getPrefix() + "§c/verify <DiscordId>"));
             }
